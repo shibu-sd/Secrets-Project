0719e6a3c990590be4a59803003043f998b8a9aa
shibu-sd <sd.shibu007@gmail.com>
2023-08-02 16:45:21 +0530

    initial-commit

---
+++ app.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
+const md5 = require("md5");
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB", {useNewUrlParser : true}, function(err){
+    if (err) console.log(err);
+    else console.log("Successfully connected to MongoDB");
+});
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email : req.body.username,
+        password : md5(req.body.password)
+    });
+
+    newUser.save(function(err){
+        if (err) console.log(err);
+        else res.render("secrets");
+    });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
+    User.findOne({email : username}, function(err, foundUser){
+        if (err) console.log(err);
+        else
+        {
+            if (foundUser)
+            {
+                if (foundUser.password === password)
+                    res.render("secrets");
+            }
+        }
+    })
+})
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+});
